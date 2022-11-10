b1831e7b6e1308d0ee21e1e62e66a10b506d9bee
Vlad-Kolyada <107812458+daddy-our-bandera@users.noreply.github.com>
2022-11-11 00:44:59 +0200

    1-2_exercise

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Change code below this line// Описан в документации
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createMarkUp = galleryItems
+  .map(
+    ({ preview, original, description }) => `<a class="gallery__item"
+       href="${original}">
+       <img class="gallery__image"
+       src="${preview}"
+       alt="${description}" />
+</a>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', createMarkUp);
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
