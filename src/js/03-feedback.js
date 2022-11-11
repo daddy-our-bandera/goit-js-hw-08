979a027719649864162daa287286b7ce21048e3c
Vlad-Kolyada <107812458+daddy-our-bandera@users.noreply.github.com>
2022-11-11 19:55:28 +0200

    fix_ex-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,17 +2,21 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = "feedback-form-state";
 const feedbackForm = document.querySelector(".feedback-form");
-const formData = {};
+const formData = {
+    "email": "",
+    "message": ""};
 
 feedbackForm.addEventListener("input", throttle(onInputForm,500));
 feedbackForm.addEventListener("submit",onSubForm);
 addText();
 
 
-function onInputForm(evt){
-formData[evt.target.name] = evt.target.value
 
-localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+function onInputForm(){
+formData.email = feedbackForm.email.value;
+formData.message = feedbackForm.message.value;
+
+localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 };
 
 function onSubForm(e){
@@ -23,9 +27,10 @@ function onSubForm(e){
 };
 
 function addText(){
-    if (localStorage.getItem(STORAGE_KEY)) {
-        feedbackForm.email.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).email || "";
-        feedbackForm.message.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).message || "";
+    let saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (saveData) {
+        feedbackForm.email.value = saveData.email || "";
+        feedbackForm.message.value = saveData.message || "";
     };
 
 };
